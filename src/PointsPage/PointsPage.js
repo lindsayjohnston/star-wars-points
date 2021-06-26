7a37a91057f2c2c369e0ff67a0624c190dc4953d
Lindsay Johnston <lindsaykjohnston@outlook.com>
2021-06-26 16:30:46 -0500

    Set min-width to 300px for all pages

--- src/PointsPage/PointsPage.js
+++ src/PointsPage/PointsPage.js
@@ -21,12 +21,13 @@ const pointsPage = (props) => {
         <div className={styles.PointsPage}>
             <h1>Customer Loyalty Points Program</h1>
             <p className={styles.Instructions}>You may use dummy data or upload a CSV file of your own. We've provided an example <a href="https://lindsayjohnston.github.io/star-wars-csv-download/" target="_blank" rel="noreferrer">CSV File</a> if you'd like to try the upload function! </p>
+            {pointsDiv}
             <div className={styles.ButtonDiv}>
                 <button onClick={props.dummyData}>Use Dummy Data</button>
             </div>
             <h2>--OR--</h2>
             <Reader dataLoaded={(data) => props.dataLoaded(data)} />
-            {pointsDiv}
+            
         </div>
     )
 }
