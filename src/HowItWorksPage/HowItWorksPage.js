789835e40d138f2ea721fd9f3b4617c83e0ab65c
Lindsay Johnston <lindsaykjohnston@outlook.com>
2022-01-17 16:59:50 -0800

    Added loyalty welcome and style for h2

--- src/HowItWorksPage/HowItWorksPage.js
+++ src/HowItWorksPage/HowItWorksPage.js
@@ -6,7 +6,9 @@ const howItWorksPage = () => {
 
     return (
         <div className={styles.PointsPage}>
-            <h1>How StarPoints are Calulated</h1>
+            <h1>StarPoints Customer Loyalty Program</h1>
+            <p>Thank you for being a loyal customer! We are happy to reward EVERY PURCHASE with StarPoints that you can redeem for stellar trips to anywhere in the universe!</p>
+            <h2>How StarPoints are Calulated</h2>
             <p className={styles.Instructions}>
                 <li>
                     Customers receive 2 StarPoints for every dollar spent over $100 in each transaction, plus 1 point for every dollar spent over $50 in each transaction.
